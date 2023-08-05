ed66bf5e0f769ea099caa111cd6bca2250e9b564
Bahadır Can <bahadircan.bc@gmail.com>
2023-08-05 23:40:10 +0300

    added grid cols

--- src/App.jsx
+++ src/App.jsx
@@ -40,39 +40,74 @@ let NodeJSON = [
 ];
 
 function Node(params) {
-
   const onClick = () => {
     if (!params.active) return;
 
     // console.log(NodeJSON[params.id]);
     NodeJSON[params.id].completed = !NodeJSON[params.id].completed;
-    
+
     // NodeJSON[params.id].active = NodeJSON[params.id].parentID?.every((id) => {return NodeJSON[id].completed});
     NodeJSON.forEach((node) => {
       if (node.parentID === null) return;
-      node.active = node.parentID?.every((id) => {return NodeJSON[id].completed});
+      node.active = node.parentID?.every((id) => {
+        return NodeJSON[id].completed;
+      });
     });
-      
   };
 
   return (
     <div
-      className={`${params.active ? (params.completed ? 'bg-green-500' : 'bg-red-500') : 'bg-gray-500'} w-10 aspect-square rounded-full`}
+      style={{ gridColumn: params.col }}
+      className={`${
+        params.active
+          ? params.completed
+            ? "bg-green-500"
+            : "bg-red-500"
+          : "bg-gray-500"
+      } w-10 aspect-square rounded-full`}
       onClick={onClick}
-    >{params.id}</div>
+    >
+      {params.id}
+    </div>
   );
 }
 
 function App() {
   // const startNode = React.useRef(null);
   const [nodes, setNodes] = React.useState([]);
+  let nodesBuilt = [];
+  let lastGridPos = [1, 1];
   const [rerender, setRerender] = React.useState(false);
 
+  const buildNodes = (id, col) => {
+    if (nodesBuilt.includes(id)) return;
+    const node = NodeJSON.find((node) => node.id === id);
+    const nodeObj = (
+      <Node
+        col={col}
+        key={node.id}
+        id={node.id}
+        active={node.active}
+        completed={node.completed}
+      />
+    );
+    setNodes((nodes) => [...nodes, nodeObj]);
+    nodesBuilt.push(id);
+    // nodes.push(<Node row={lastGridPos[0]} col={lastGridPos[1]} key={node.id} id={node.id} active={node.active} completed={node.completed}/>);
+    node.childrenID.forEach((id) => buildNodes(id, col + 1));
+  };
+
   useEffect(() => {
-    setNodes(NodeJSON.map((node) => <Node key={node.id} id={node.id} active={node.active} completed={node.completed}/>));
+    setNodes([]);
+    buildNodes(0, 1);
+    // setNodes(NodeJSON.map((node) => {setLastGridPos([lastGridPos[0]+1, lastGridPos[1]+1]) ;return <Node row={lastGridPos[0]} col={lastGridPos[1]} key={node.id} id={node.id} active={node.active} completed={node.completed}/>}));
   }, [rerender]);
 
-  return <div className="grid" onClick={()=>setRerender(!rerender)}>{nodes}</div>;
+  return (
+    <div className="grid" onClick={() => setRerender(!rerender)}>
+      {nodes}
+    </div>
+  );
 }
 
 export default App;
